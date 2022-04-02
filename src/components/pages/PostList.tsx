374684edf4c4ff12ed8ad560a7c52bf4002a914f
Dario Kriskovic <dario.kriskovic@gmail.com>
2022-04-02 16:37:24 +0200

    iplement post search by author

--- src/components/pages/PostList.tsx
+++ src/components/pages/PostList.tsx
@@ -1,7 +1,7 @@
 import { PostItem } from 'components/shared/PostItem/PostItem';
 import { useFetcher } from 'hooks/useFetcher';
 import { useStore } from 'hooks/useStore';
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useMemo, useState } from 'react';
 import { Post } from 'store/models/post';
 import { ActionType } from 'store/store';
 import useSWR from 'swr';
@@ -12,6 +12,7 @@ interface IPostListProps {
 
 export const PostList: FC<IPostListProps> = ({ message }) => {
   console.log(`${message} PostList`);
+  const [searchValue, setSearchValue] = useState('');
   const [store, dispatch] = useStore() as Array<any>;
   const fetcher = useFetcher();
 
@@ -32,14 +33,31 @@ export const PostList: FC<IPostListProps> = ({ message }) => {
     }
   }, [dispatch, postsResponse]);
 
+  const filteredPosts = useMemo(
+    () =>
+      store.posts.filter((post: Post) =>
+        post.user?.name?.toLowerCase().includes(searchValue.trim()),
+      ),
+    [searchValue, store.posts],
+  );
+
   return (
     <main className="container mt-10 px-4">
-      <h1 className="mb-4 font-bold text-xl text-center">Posts</h1>
+      <div className="flex mb-4 items-center justify-between">
+        <h1 className="font-bold text-xl text-left">Posts</h1>
+        <input
+          value={searchValue}
+          type="text"
+          className="form-input grow-0 shrink basis-0 px-3 py-2 rounded border-gray-200 text-left w-72"
+          placeholder="search authors"
+          onChange={(event: any) => setSearchValue(event.target.value)}
+        />
+      </div>
       {!postsResponse && !postsError ? (
         <div>Loading</div>
       ) : (
         <>
-          {store.posts.map((post: Post) => (
+          {filteredPosts.map((post: Post) => (
             <PostItem key={post.id} post={post} message={message} />
           ))}
         </>
